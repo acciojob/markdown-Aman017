61ee26c8651c3f661590824c2770085bf62c2a50
Aman Malik <amanmalik0135@gmail.com>
2025-05-06 21:54:58 +0530

    hey

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,37 @@
-import React from "react";
-import MarkdownEditor from "./MarkdownEditor";
-import './styles.css';
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import "./App.css";
 
+function Markdown() {
+  const [markdown, setMarkdown] = useState("");
+  const [loading, setLoading] = useState(true);
 
-const App = () => {
-  
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
 
- 
+  const changeLoadingVar = () => {
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    setTimeout(changeLoadingVar, 3000);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="app">
-  
-    <MarkdownEditor />
-  </div>
+      <textarea className="textarea" onChange={handleChange} />
+      <ReactMarkdown className="preview">{markdown}</ReactMarkdown>
+    </div>
   );
-};
+}
 
-export default App;
+export default Markdown;
